2810f3ec224c299864b6df03e808f39c5d865b12
David Johnson <dcharles525@gmail.com>
2025-05-07 22:54:04 -0400

    Updated nixos config to enable flakes/home manager
    Added hyprland config

---
+++ home.nix
@@ -0,0 +1,266 @@
+{ config, pkgs, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "d";
+  home.homeDirectory = "/home/d";
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "24.11"; # Please read the comment before changing.
+
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+  home.packages = [
+    # # Adds the 'hello' command to your environment. It prints a friendly
+    # # "Hello, world!" when run.
+    # pkgs.hello
+
+    # # It is sometimes useful to fine-tune packages, for example, by applying
+    # # overrides. You can do that directly here, just don't forget the
+    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
+    # # fonts?
+    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+
+    # # You can also create simple shell scripts directly inside your
+    # # configuration. For example, this adds a command 'my-hello' to your
+    # # environment:
+    # (pkgs.writeShellScriptBin "my-hello" ''
+    #   echo "Hello, ${config.home.username}!"
+    # '')
+  ];
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
+
+  # Home Manager can also manage your environment variables through
+  # 'home.sessionVariables'. These will be explicitly sourced when using a
+  # shell provided by Home Manager. If you don't want to manage your shell
+  # through Home Manager then you have to manually source 'hm-session-vars.sh'
+  # located at either
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/d/etc/profile.d/hm-session-vars.sh
+  #
+  home.sessionVariables = {
+    # EDITOR = "emacs";
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  wayland.windowManager.hyprland = {
+    enable = true;
+    extraConfig = ''
+      monitor = eDP-1, 1920x1080@144, 0x0, 1
+
+      $terminal = ghostty
+      $fileManager = dolphin
+      $menu = wofi --show drun
+
+      exec-once=bash ~/.config/hypr/start.sh
+
+      env = XCURSOR_SIZE,24
+      env = HYPRCURSOR_SIZE,24
+
+      general {
+        gaps_in = 5
+        gaps_out = 20
+
+        border_size = 2
+
+        col.active_border = rgba(33ccffee) rgba(00ff99ee) 45deg
+        col.inactive_border = rgba(595959aa)
+
+        resize_on_border = false
+
+        allow_tearing = false
+
+        layout = dwindle
+      }
+
+      decoration {
+        rounding = 10
+
+        active_opacity = 1.0
+        inactive_opacity = 1.0
+
+        shadow {
+          enabled = true
+          range = 4
+          render_power = 3
+          color = rgba(1a1a1aee)
+        }
+
+        blur {
+          enabled = true
+          size = 3
+          passes = 1
+
+          vibrancy = 0.1696
+        }
+      }
+
+      animations {
+        enabled = yes, please :)
+
+        bezier = easeOutQuint,0.23,1,0.32,1
+        bezier = easeInOutCubic,0.65,0.05,0.36,1
+        bezier = linear,0,0,1,1
+        bezier = almostLinear,0.5,0.5,0.75,1.0
+        bezier = quick,0.15,0,0.1,1
+
+        animation = global, 1, 10, default
+        animation = border, 1, 5.39, easeOutQuint
+        animation = windows, 1, 4.79, easeOutQuint
+        animation = windowsIn, 1, 4.1, easeOutQuint, popin 87%
+        animation = windowsOut, 1, 1.49, linear, popin 87%
+        animation = fadeIn, 1, 1.73, almostLinear
+        animation = fadeOut, 1, 1.46, almostLinear
+        animation = fade, 1, 3.03, quick
+        animation = layers, 1, 3.81, easeOutQuint
+        animation = layersIn, 1, 4, easeOutQuint, fade
+        animation = layersOut, 1, 1.5, linear, fade
+        animation = fadeLayersIn, 1, 1.79, almostLinear
+        animation = fadeLayersOut, 1, 1.39, almostLinear
+        animation = workspaces, 1, 1.94, almostLinear, fade
+        animation = workspacesIn, 1, 1.21, almostLinear, fade
+        animation = workspacesOut, 1, 1.94, almostLinear, fade
+      }
+
+      dwindle {
+        pseudotile = true 
+        preserve_split = true # You probably want this
+      }
+
+      master {
+        new_status = master
+      }
+
+      misc {
+        force_default_wallpaper = -1 
+        disable_hyprland_logo = true 
+      }
+
+      input {
+        kb_layout = us
+        kb_variant =
+        kb_model =
+        kb_options =
+        kb_rules =
+
+        follow_mouse = 1
+
+        sensitivity = 0 # -1.0 - 1.0, 0 means no modification.
+
+        touchpad {
+          natural_scroll = true
+        }
+      }
+
+      gestures {
+        workspace_swipe = false
+      }
+
+      device {
+        name = epic-mouse-v1
+        sensitivity = -0.5
+      }
+
+      $mainMod = SUPER # Sets "Windows" key as main modifier
+
+      bind = $mainMod, Q, exec, $terminal
+      bind = $mainMod, C, killactive,
+      bind = $mainMod, M, exit,
+      bind = $mainMod, E, exec, $fileManager
+      bind = $mainMod, V, togglefloating,
+      bind = $mainMod, R, exec, $menu
+      bind = $mainMod, P, pseudo, # dwindle
+      bind = $mainMod, J, togglesplit, # dwindle
+
+      bind = $mainMod, left, movefocus, l
+      bind = $mainMod, right, movefocus, r
+      bind = $mainMod, up, movefocus, u
+      bind = $mainMod, down, movefocus, d
+
+      bind = $mainMod, 1, workspace, 1
+      bind = $mainMod, 2, workspace, 2
+      bind = $mainMod, 3, workspace, 3
+      bind = $mainMod, 4, workspace, 4
+      bind = $mainMod, 5, workspace, 5
+      bind = $mainMod, 6, workspace, 6
+      bind = $mainMod, 7, workspace, 7
+      bind = $mainMod, 8, workspace, 8
+      bind = $mainMod, 9, workspace, 9
+      bind = $mainMod, 0, workspace, 10
+
+      bind = $mainMod SHIFT, 1, movetoworkspace, 1
+      bind = $mainMod SHIFT, 2, movetoworkspace, 2
+      bind = $mainMod SHIFT, 3, movetoworkspace, 3
+      bind = $mainMod SHIFT, 4, movetoworkspace, 4
+      bind = $mainMod SHIFT, 5, movetoworkspace, 5
+      bind = $mainMod SHIFT, 6, movetoworkspace, 6
+      bind = $mainMod SHIFT, 7, movetoworkspace, 7
+      bind = $mainMod SHIFT, 8, movetoworkspace, 8
+      bind = $mainMod SHIFT, 9, movetoworkspace, 9
+      bind = $mainMod SHIFT, 0, movetoworkspace, 10
+
+      bind = $mainMod SHIFT, L, moveactive, 30 0
+      bind = $mainMod SHIFT, H, moveactive, -30 0
+      bind = $mainMod SHIFT, K, moveactive, 0 -30
+      bind = $mainMod SHIFT, J, moveactive, 0 30
+
+      bind = $mainMod, S, togglespecialworkspace, magic
+      bind = $mainMod SHIFT, S, movetoworkspace, special:magic
+
+      bind = $mainMod, mouse_down, workspace, e+1
+      bind = $mainMod, mouse_up, workspace, e-1
+
+      bind = $mainMod, S, exec, rofi -show drun -show-icons
+
+      bindm = $mainMod, mouse:272, movewindow
+      bindm = $mainMod, mouse:273, resizewindow
+
+      bindel = ,XF86AudioRaiseVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+
+      bindel = ,XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-
+      bindel = ,XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle
+      bindel = ,XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle
+      bindel = ,XF86MonBrightnessUp, exec, brightnessctl s 10%+
+      bindel = ,XF86MonBrightnessDown, exec, brightnessctl s 10%-
+
+      bindl = , XF86AudioNext, exec, playerctl next
+      bindl = , XF86AudioPause, exec, playerctl play-pause
+      bindl = , XF86AudioPlay, exec, playerctl play-pause
+      bindl = , XF86AudioPrev, exec, playerctl previous
+
+      windowrulev2 = suppressevent maximize, class:.*
+
+      windowrulev2 = nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0
+    '';
+  };
+}
