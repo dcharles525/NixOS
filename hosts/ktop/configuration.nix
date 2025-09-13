4c748860f647cc2c3e97076bf68c4af214e0e4ba
David Johnson <dcharles525@gmail.com>
2025-09-13 17:10:15 -0400

    Clean up configuration file

--- hosts/ktop/configuration.nix
+++ hosts/ktop/configuration.nix
@@ -1,88 +1,75 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
-
 { config, pkgs, lib, inputs, ... }:
 
 {
+  #
+  # Config Setup
+  #
+
   imports = [
     ./hardware-configuration.nix
     ./app-configs/vim.nix
     inputs.home-manager.nixosModules.default
   ];
-
   nixpkgs = {
     config = {
       allowUnfree = true;
     };
   };
-
   nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
     "1password-gui"
     "1password"
   ];
-
-  nixpkgs.buildPlatform = {
-    system = "x86_64-linux";
+  environment.variables = {
+    EDITOR = "vim";
+    PATH="$HOME/go/bin:$PATH";
+  };
+  environment.sessionVariables = {
+    NIXOS_OZONE_WL = "1";
+    ADW_DISABLE_PORTAL = "1";
+  };
+  programs.vim.defaultEditor = true;
+  system.stateVersion = "24.11";
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  virtualisation.docker.enable = true;
+  virtualisation.docker.daemon.settings = {
+      dns = [ "1.1.1.1" ];
   };
 
-  # Bootloader.
+  #
+  # Bootloader
+  #
+
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.kernelParams = [ "systemd.unified_cgroup_hierarchy=1" ];
 
-  services.displayManager.ly.enable = true;
+  # User Setup
+
+  users.users.d = {
+    isNormalUser = true;
+    description = "David Johnson";
+    extraGroups = [ "networkmanager" "wheel" "docker" ];
+  };
+  home-manager = {
+    backupFileExtension = "backup";
+    extraSpecialArgs = { inherit inputs; };
+    users = {
+      "d" = import ./home.nix;
+    };
+  };
+
+  #
+  # Networking
+  #
 
   networking.hostName = "d";
   networking.networkmanager.enable = true;
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
   networking.wireless.iwd.enable = true;
-
-  # Set your time zone.
-  time.timeZone = "America/New_York";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_US.UTF-8";
-    LC_IDENTIFICATION = "en_US.UTF-8";
-    LC_MEASUREMENT = "en_US.UTF-8";
-    LC_MONETARY = "en_US.UTF-8";
-    LC_NAME = "en_US.UTF-8";
-    LC_NUMERIC = "en_US.UTF-8";
-    LC_PAPER = "en_US.UTF-8";
-    LC_TELEPHONE = "en_US.UTF-8";
-    LC_TIME = "en_US.UTF-8";
-  };
-
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Enable the Pantheon Desktop Environment.
-  # services.xserver.displayManager.lightdm.enable = true;
-  # services.xserver.desktopManager.pantheon.enable = true;
-
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
-  };
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
 
-  hardware.graphics = {
-    enable = true;
-  };
-
+  # Enable and configure bluetooth
   hardware.bluetooth = {
     enable = true;
     settings = {
@@ -98,120 +85,40 @@
     };
   };
 
-  # Enable sound with pipewire.
-  services.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
+  networking.firewall.enable = true;
 
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
+  #
+  # Time and Internationalisation
+  #
+
+  time.timeZone = "America/New_York";
+  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
   };
 
+  #
+  # Display and DE Enablement
+  #
+
+  services.displayManager.ly.enable = true;
+  services.xserver.enable = true;
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+  hardware.graphics = {
+    enable = true;
+  };
   security.pam.services.hyprlock = {};
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.d = {
-    isNormalUser = true;
-    description = "David Johnson";
-    extraGroups = [ "networkmanager" "wheel" "docker" ];
-  };
-
-  home-manager = {
-    backupFileExtension = "backup";
-    extraSpecialArgs = { inherit inputs; };
-    users = {
-      "d" = import ./home.nix;
-    };
-  };
-
-  environment.variables = {
-    EDITOR = "vim";
-    PATH="$HOME/go/bin:$PATH";
-  };
-  environment.sessionVariables = {
-    NIXOS_OZONE_WL = "1";
-    ADW_DISABLE_PORTAL = "1";
-  };
-
-  nixpkgs.overlays = [
-    (final: prev: {
-      postman = prev.postman.overrideAttrs(old: rec {
-        version = "20231205182607";
-        src = final.fetchurl {
-          url = "https://web.archive.org/web/${version}/https://dl.pstmn.io/download/latest/linux_64";
-          sha256 = "sha256-PthETmSLehg6eWpdDihH1juwiyZdJvzr+qyG2YYuEZI=";
-          name = "${old.pname}-${version}.tar.gz";
-        };
-      });
-    })
-  ];
-
-  programs.vim.defaultEditor = true;
-  environment.systemPackages = with pkgs; [
-    vim_configurable
-    git
-    tmux
-    k9s
-    go
-    gum
-    ghostty
-    yarn
-    nodejs
-    circumflex
-    tailscale
-    waybar
-    dunst
-    libnotify
-    swww
-    rofi-wayland
-    nautilus
-    iwd
-    k3d
-    gnumake
-    minikube
-    slack
-    kubectl
-    socat
-    kubernetes-helm
-    jq
-    wget
-    zip
-    unzip
-    xorg.xhost
-    gparted
-    rpi-imager
-    partclone
-    postman
-    hyprpaper
-    hyprshot
-    qimgv
-    libreoffice
-    mariadb
-    dbeaver-bin
-    rofi-bluetooth
-    catppuccin-cursors.mochaMauve
-    docker-buildx
-    inputs.iwmenu.packages.${pkgs.system}.default
-  ];
-
-  programs._1password.enable = true;
-  programs._1password-gui = {
-    enable = true;
-    # Certain features, including CLI integration and system authentication support,
-    # require enabling PolKit integration on some desktop environments (e.g. Plasma).
-    polkitPolicyOwners = [ "d" ];
-  };
-
   fonts.packages = with pkgs; [
     fira-code
     fira-code-symbols
@@ -223,13 +130,6 @@
     noto-fonts-emoji
     proggyfonts
   ] ++ builtins.filter lib.attrsets.isDerivation (builtins.attrValues pkgs.nerd-fonts);
-
-  services.tailscale.enable = true;
-
-  programs.vim.enable = true;
-
-  # Install firefox.
-  programs.firefox.enable = true;
 
   programs.hyprland = {
     enable = true;
@@ -247,52 +147,99 @@
   xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
   xdg.portal.configPackages = [ pkgs.xdg-desktop-portal-gtk ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+  #
+  # Enable sound with pipewire.
+  #
 
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.11"; # Did you read the comment?
-
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
-  virtualisation.docker.enable = true;
-  virtualisation.docker.daemon.settings = {
-      dns = [ "1.1.1.1" ];
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
   };
-  #virtualisation.docker.rootless = {
-  #  enable = true;
-  #  setSocketVariable = true;
-  #  daemon.settings = {
-  #    "exec-opts" = [ "native.cgroupdriver=cgroupfs" ];
-  #  };
-  #};
 
-  #systemd.user.services.docker = {
-    # Set the Delegate property for the user service
-  #  serviceConfig.Delegate = lib.mkForce "cpu cpuset io memory pids";
-    # Alternative for maximum delegation:
-    # serviceConfig.Delegate = "yes";
-  #};
+  #
+  # App Configuration
+  #
 
+  nixpkgs.overlays = [
+    (final: prev: {
+      postman = prev.postman.overrideAttrs(old: rec {
+        version = "20231205182607";
+        src = final.fetchurl {
+          url = "https://web.archive.org/web/${version}/https://dl.pstmn.io/download/latest/linux_64";
+          sha256 = "sha256-PthETmSLehg6eWpdDihH1juwiyZdJvzr+qyG2YYuEZI=";
+          name = "${old.pname}-${version}.tar.gz";
+        };
+      });
+    })
+  ];
+  
+  environment.systemPackages = with pkgs; [
+    # Developer Tools
+    vim_configurable
+    git
+    tmux
+    k9s
+    go    
+    yarn
+    nodejs
+    tailscale
+    k3d
+    minikube
+    gnumake
+    kubectl
+    socat
+    kubernetes-helm
+    jq
+    wget
+    zip
+    unzip
+    mariadb
+    dbeaver-bin
+    docker-buildx
+
+    # Computer Environment
+    waybar
+    gum
+    ghostty
+    xorg.xhost
+    hyprpaper
+    hyprshot
+    rofi-bluetooth
+    catppuccin-cursors.mochaMauve
+    inputs.iwmenu.packages.${pkgs.system}.default
+
+    # Media
+    circumflex
+    dunst
+    libnotify
+    swww
+    rofi-wayland
+    nautilus
+    iwd
+    slack
+    
+    # Misc Apps    
+    gparted
+    rpi-imager
+    partclone
+    postman
+    qimgv
+    libreoffice
+  ];
+
+  programs._1password.enable = true;
+  programs._1password-gui = {
+    enable = true;
+    # Certain features, including CLI integration and system authentication support,
+    # require enabling PolKit integration on some desktop environments (e.g. Plasma).
+    polkitPolicyOwners = [ "d" ];
+  };
+
+  services.tailscale.enable = true;
+  programs.vim.enable = true;
+  programs.firefox.enable = true;
 }
