0fcb7aa6c1a0790e06d1a7cc1e24ae438a9d72cc
David Johnson <dcharles525@gmail.com>
2026-02-05 21:45:06 -0500

    feat(Add vim line and tmux config):

--- hosts/ktop/configuration.nix
+++ hosts/ktop/configuration.nix
@@ -323,6 +323,19 @@
 
   nixpkgs.config.android_sdk.accept_license = true;
 
+  programs.tmux = {
+    enable = true;
+    extraConfig = ''
+      set -g status-position bottom
+      
+      set -g status-left-length 30
+      set -g status-right-length 60
+      set -g status-right "#(free -h | awk '/Mem:/ {print $3\"/\"$2}') | #(grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {printf \"%.0f%%\", usage}') | %H:%M %d-%b"
+      
+      set-hook -g session-created 'split-window -h ; select-pane -L ; split-window -v ; select-pane -R'
+    '';
+  };
+
   programs.gnupg.agent = {
     enable = true;
     enableSSHSupport = true;
